38f77ec45067a0a5346d4398ebec8f30b94cba0a
itskhurram <itskhurrams@outlook.com>
2021-05-23 20:15:53 +0500

    app URL also has been changed

--- AngularApps/projects/app01/src/app/app-routing.module.ts
+++ AngularApps/projects/app01/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -13,4 +13,19 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private route:Router){
+  var topHref = window.top.location.href != window.location.href ?
+                window.top.location.href.substring(0,
+                                         window.top.location.href.indexOf('/AngularSpa') + 11) :
+                null;
+  this.route.events.subscribe(e => {
+    if(e instanceof NavigationEnd){
+      if (topHref){
+        window.top.history.replaceState(window.top.history.state,
+                                        window.top.document.title, topHref + e.url);
+      }
+    }
+  });
+}
+}
